5061af33f97bebcca93026fc9a0f604a87b81676
kangyongbin <byyb2696@gmail.com>
2023-06-17 17:43:19 +0900

    6/17

--- src/main/java/com/cos/blog/test/HttpComtrollerTest.java
+++ src/main/java/com/cos/blog/test/HttpComtrollerTest.java
@@ -15,6 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpComtrollerTest {
 
+	private static final String TAG = "HttpControllerTest : ";
+
+	// localhost:8000/blog/http/lombok
+	@GetMapping("http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+		System.out.println(TAG + "getter : " + m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG + "setter : " + m.getUsername());
+		return "lombok test 완료";
+
+	}
+
 	// 인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
 	// http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
